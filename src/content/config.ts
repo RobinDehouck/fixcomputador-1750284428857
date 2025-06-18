cf4164d0f43afa9fa548b538b9feba10a04e4a0c
Robin Dehouck <59605143+RobinDehouck@users.noreply.github.com>
2025-06-18 19:31:21 -0300

    Add content configuration for Astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,70 +1,17 @@
 import { z, defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
 
-const metadataDefinition = () =>
-  z
-    .object({
-      title: z.string().optional(),
-      ignoreTitleTemplate: z.boolean().optional(),
-
-      canonical: z.string().url().optional(),
-
-      robots: z
-        .object({
-          index: z.boolean().optional(),
-          follow: z.boolean().optional(),
-        })
-        .optional(),
-
-      description: z.string().optional(),
-
-      openGraph: z
-        .object({
-          url: z.string().optional(),
-          siteName: z.string().optional(),
-          images: z
-            .array(
-              z.object({
-                url: z.string(),
-                width: z.number().optional(),
-                height: z.number().optional(),
-              })
-            )
-            .optional(),
-          locale: z.string().optional(),
-          type: z.string().optional(),
-        })
-        .optional(),
-
-      twitter: z
-        .object({
-          handle: z.string().optional(),
-          site: z.string().optional(),
-          cardType: z.string().optional(),
-        })
-        .optional(),
-    })
-    .optional();
-
-const postCollection = defineCollection({
-  loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/data/post' }),
+const blogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
-    publishDate: z.date().optional(),
-    updateDate: z.date().optional(),
-    draft: z.boolean().optional(),
-
     title: z.string(),
-    excerpt: z.string().optional(),
-    image: z.string().optional(),
-
-    category: z.string().optional(),
+    description: z.string().optional(),
+    pubDate: z.date().optional(),
+    updatedDate: z.date().optional(),
+    heroImage: z.string().optional(),
     tags: z.array(z.string()).optional(),
-    author: z.string().optional(),
-
-    metadata: metadataDefinition(),
   }),
 });
 
 export const collections = {
-  post: postCollection,
+  'blog': blogCollection,
 };
